c13161f1e9c29972d2521d51b45fd7d2a0ae1af0
이지훈 <ljihoon0330@gmail.com>
2020-10-27 10:59:55 +0900

    github-progiles

---
+++ github-profiles/index.js
@@ -0,0 +1,48 @@
+const url = `https://api.github.com/users/`;
+
+const getUser = async (user = "JiHoon-0330") => {
+  const state = {
+    error: `failed loading...`,
+    data: null
+  };
+
+  state.data = await fetch(`${url}${user}`)
+    .then(res => {
+      if (!res.ok) {
+        console.log(`res error`);
+        return;
+      } else {
+        state.error = false;
+        return res.json();
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      return;
+    });
+
+  return state;
+};
+
+const createUserCard = async () => {
+  const response = await getUser();
+  console.log(response);
+  const { data, error } = response;
+  const { avatar_url, name, bio, followers, public_repos } = data;
+  document.querySelector("main").innerHTML = `
+  <span class="img" style="background-image:url(${avatar_url})"></span>
+  <div class="user-info">
+    <h3 class="profile">github-profile</h3>
+    <span class="name">name: ${name}</span>
+    <span class="bio">bio: ${bio}</span>
+    <span class="followers">followers: ${followers}</span>
+    <span class="public_repos">public_repos: ${public_repos}</span>
+  </div>
+  `;
+};
+
+const init = () => {
+  createUserCard();
+};
+
+init();
