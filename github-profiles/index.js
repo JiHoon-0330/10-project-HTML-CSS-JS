61ab19e779c8e1998e7fdd7e8095d9377624ea65
이지훈 <ljihoon0330@gmail.com>
2020-10-28 11:31:48 +0900

    프로필 링크 추가

--- github-profiles/index.js
+++ github-profiles/index.js
@@ -1,8 +1,8 @@
 const url = `https://api.github.com/users/`;
 
-const getUser = async (user = "JiHoon-0330") => {
+const getUser = async user => {
   const state = {
-    error: `failed loading...`,
+    error: `failed to loading...`,
     data: null
   };
 
@@ -24,19 +24,26 @@ const getUser = async (user = "JiHoon-0330") => {
   return state;
 };
 
-const createUserCard = async user => {
+const createUserCard = async (user = "JiHoon-0330") => {
   const response = await getUser(user);
+  const main = document.querySelector("main");
   console.log(response);
   const { data, error } = response;
+  if (error) {
+    main.innerHTML = `<p class="error-message">${error}</p>`;
+    return;
+  }
   const { avatar_url, name, bio, followers, public_repos } = data;
-  document.querySelector("main").innerHTML = `
-  <span class="img" style="background-image:url(${avatar_url})"></span>
-  <div class="user-info">
-    <h3 class="profile">github-profile</h3>
-    <span class="name">name: ${name}</span>
-    <span class="bio">bio: ${bio}</span>
-    <span class="followers">followers: ${followers}</span>
-    <span class="public_repos">public_repos: ${public_repos}</span>
+  main.innerHTML = `
+  <div class="container">
+    <a href="http://www.github.com/${user}"><span class="img" style="background-image:url(${avatar_url})"></span></a>
+    <div class="user-info">
+      <h3 class="profile">github-profile</h3>
+      <span class="name">name: ${name}</span>
+      <span class="bio">bio: ${bio}</span>
+      <span class="followers">followers: ${followers}</span>
+      <span class="public_repos">public_repos: ${public_repos}</span>
+    </div>
   </div>
   `;
 };
